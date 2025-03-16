76c9012ae84feb52a0e66ab86ce1a2a86a6cda3f
DevIke <mike.hneise@gmail.com>
2025-03-16 15:39:31 +0100

    added constraints to delete all conected data

--- src/main/resources/sql/tables/tblEpisode.sql
+++ src/main/resources/sql/tables/tblEpisode.sql
@@ -3,5 +3,7 @@ create table if not exists episode (    nKey           integer primary key,
                                         nEpisodeNumber integer,
                                         szTitle        varchar(255),
                                         szURL          varchar(255),
-                                        bLoaded        int
+                                        bLoaded        int,
+                                        INDEX `FK_episode_season` (`nSeasonLink`) USING BTREE,
+                                        CONSTRAINT `FK_episode_season` FOREIGN KEY (`nSeasonLink`) REFERENCES `season` (`nKey`) ON UPDATE NO ACTION ON DELETE CASCADE
 );
